52cd2457eea91ed821a0cd49242a3636cd653261
MasonBartell <masonjbartell@gmail>
2023-01-24 09:43:10 -0600

    Made a comment in intelliJ

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLOutput;
+
 public class Main {
 
 
         public static void main(String[] args)
         {
           System.out.println("made a comment in replit");
+            System.out.println("in intellJ");
             JFrame myFrame = new JFrame();
             myFrame.setSize(500,500);
             AnimateFrame myPanel = new AnimateFrame();
